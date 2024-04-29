84e33ecb34e6c8baea10f7afdfb41077d7bc7690
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-04-30 02:16:30 +0600

    add animation

--- src/Component/CountryCard.jsx
+++ src/Component/CountryCard.jsx
@@ -6,7 +6,7 @@ const CountryCard = ({spot}) => {
     const {image,country_Name} =spot;
      
     return (
-        <Fade cascade={false} direction="up" delay={.5} triggerOnce={false}>
+        <Fade cascade={false} direction="up" delay={.5} triggerOnce={true}>
              <Link  to={`/country/${country_Name}`} className="md:w-[300px] w-full h-[200px] bg-cover bg-center text-center flex items-end justify-center border-2 border-indigo-500 rounded-xl " style={{ backgroundImage: `url(${image})`}}>
             
         </Link>
