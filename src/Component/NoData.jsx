b1aa9481eb78445606af5b5d82687fdcfd84fe3a
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-04-30 01:50:24 +0600

    no data design complete

--- src/Component/NoData.jsx
+++ src/Component/NoData.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
  
 
 const NoData = () => {
     return (
-        <div>
+        <div className="space-y-3 h-4/5">
             <h1 className="text-4xl font-bold text-center">Data Not Found</h1>
             <p className="text-center text-xl">Please Add Your Own Spot</p>
+            <div className="text-center"><Link to='/addtouristspot' className="btn btn-primary">
+                Add Spot
+                </Link></div>
         </div>
     );
 };
