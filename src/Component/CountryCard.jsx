74bfda36867c9dee4ee72eb6b48933a75c55339c
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-04-30 18:07:35 +0600

    add tooltip in country

--- src/Component/CountryCard.jsx
+++ src/Component/CountryCard.jsx
@@ -1,14 +1,16 @@
 import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
- 
+import { Tooltip } from "react-tooltip";
+import 'react-tooltip/dist/react-tooltip.css'
 
 const CountryCard = ({spot}) => {
     const {image,country_Name} =spot;
      
     return (
         <Fade cascade={false} direction="up" delay={.5} triggerOnce={true}>
-             <Link  to={`/country/${country_Name}`} className="md:w-[300px] w-full h-[200px] bg-cover bg-center text-center flex items-end justify-center border-2 border-indigo-500 rounded-xl " style={{ backgroundImage: `url(${image})`}}>
-            
+             <Link data-tooltip-id={country_Name}
+  data-tooltip-content={country_Name}  to={`/country/${country_Name}`} className={`md:w-[300px] w-full h-[200px] bg-cover bg-center text-center flex items-end justify-center border-2 border-indigo-500 rounded-xl ${country_Name}`} style={{ backgroundImage: `url(${image})`}}>
+             <Tooltip id={country_Name} place="top"/>
         </Link>
         </Fade>
         
