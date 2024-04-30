d386d6bda767b463c3a2528f95adf233f07e51dd
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-04-30 18:15:25 +0600

    find bug

--- src/Component/CountryCard.jsx
+++ src/Component/CountryCard.jsx
@@ -9,7 +9,7 @@ const CountryCard = ({spot}) => {
     return (
         <Fade cascade={false} direction="up" delay={.5} triggerOnce={true}>
              <Link data-tooltip-id={country_Name}
-  data-tooltip-content={country_Name}  to={`/country/${country_Name}`} className={`md:w-[300px] w-full h-[200px] bg-cover bg-center text-center flex items-end justify-center border-2 border-indigo-500 rounded-xl ${country_Name}`} style={{ backgroundImage: `url(${image})`}}>
+  data-tooltip-content={country_Name}  to={`/country/${country_Name}`} className="md:w-[300px] w-full h-[200px] bg-cover bg-center text-center flex items-end justify-center border-2 border-indigo-500 rounded-xl" style={{ backgroundImage: `url(${image})`}}>
              <Tooltip id={country_Name} place="top"/>
         </Link>
         </Fade>
